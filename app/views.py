58c6cf35537563ab9726dfd5f826ddf90f171881
mahdi gueffaz <macbook@MacBook-Pro-de-macbook.local>
2020-03-23 12:32:19 +0100

    Update the route

--- app/views.py
+++ app/views.py
@@ -1,19 +1,13 @@
-from flask import Flask, render_template
-from .data import get_data, get_new_data, get_sorted_data
+from flask import Flask, render_template, redirect, url_for
+from .data import get_data, get_new_data, get_sorted_data, get_country_name
 from flask import jsonify, json
+import dateutil.parser
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    print(get_sorted_data(get_new_data('confirmed')))
-    return render_template(
-        'index.html', 
-        confirmed=get_sorted_data(get_new_data('confirmed')),
-        death=get_sorted_data(get_new_data('deaths')),
-        recovered=get_sorted_data(get_new_data('recovered'))
-    )
-
+"""
+The api
+"""
 @app.route('/api/recovered')
 def api_recovered():
     data = get_new_data('recovered')
@@ -32,7 +26,46 @@ def api_confirmed():
     data_tuple = data['locations']
     return jsonify(sorted(data_tuple, key=lambda k: k.get('total', 0), reverse=True))
 
-#get the resume of all (detah)
+#Get all the historic recovered by country
+@app.route('/api/recovered/<country_code>')
+def api_recovered_country(country_code):
+    data = get_data('recovered')
+    for country in data['locations']:
+        if country['country_code'] == country_code.upper():
+            return jsonify({
+                'data' : country['history'],
+                'last_updated': dateutil.parser.parse(data['last_updated'])
+            })
+    else:
+        return 'No found'
+
+#Get all the historic deaths by country
+@app.route('/api/deaths/<country_code>')
+def api_deaths_country(country_code):
+    data = get_data('deaths')
+    for country in data['locations']:
+        if country['country_code'] == country_code.upper():
+            return jsonify({
+                'data' : country['history'],
+                'last_updated': dateutil.parser.parse(data['last_updated'])
+            })
+    else:
+        return 'No found'
+
+#Get all the historic confirmed by country
+@app.route('/api/confirmed/<country_code>')
+def api_confirmed_country(country_code):
+    data = get_data('confirmed')
+    for country in data['locations']:
+        if country['country_code'] == country_code.upper():
+            return jsonify({
+                'data' : country['history'],
+                'last_updated': dateutil.parser.parse(data['last_updated'])
+            })
+    else:
+        return 'No found'
+
+#Get all the detah, confirmed, recovered
 @app.route('/api/all')
 def api_all():
     # data
@@ -68,6 +101,7 @@ def api_all():
 
     return jsonify({
         'data': data,
+        'last_updated': dateutil.parser.parse(confirmed["last_updated"]),
         # Latest.
         'latest': {
             'confirmed': confirmed['total'],
@@ -75,29 +109,37 @@ def api_all():
             'recovered': recovered['total'],
         }
     })
-    
-"""
-@app.route('/all')
-def all():
-    # Get all the categories.
-    confirmed = get_data('confirmed')
-    deaths    = get_data('deaths')
-    recovered = get_data('recovered')
 
-    return jsonify({
-        # Data.
-        'confirmed': confirmed,
-        'deaths':    deaths,
-        'recovered': recovered,
-
-        # Latest.
-        'latest': {
-            'confirmed': confirmed['latest'],
-            'deaths':    deaths['latest'],
-            'recovered': recovered['latest'],
-        }
-    })
 """
+The principal route
+"""
+@app.route('/')
+def index():
+    return render_template(
+        'index.html', 
+        confirmed=get_sorted_data(get_new_data('confirmed')),
+        death=get_sorted_data(get_new_data('deaths')),
+        recovered=get_sorted_data(get_new_data('recovered'))
+    )
+
+"""
+The dashboard charts (deaths, confirmed, recovered) by country
+"""
+@app.route('/<country_code>')
+def country_stats(country_code):
+    #Get the country name
+    country_name = get_country_name(country_code)
+    if country_name != None:
+        return render_template('dashboard.html', country_code=country_code,country_name=country_name)
+
+    return redirect(url_for('index'))
+
+"""
+Not exisiting route
+"""
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for('index'))
 
 if __name__== "__main__":
-    app.run() 
+    app.run()
